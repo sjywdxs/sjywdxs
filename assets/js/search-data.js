e2285040173d1137de8fb1770d8bd8f8a8be02d9
sjywdxs <55956299+sjywdxs@users.noreply.github.com>
2025-01-01 05:06:46 +0000

    Deploying to gh-pages from @ sjywdxs/sjywdxs@84a1b1a9a37fa700b2a4a4efe7a3749cffcefc6f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "Teaching",
-          description: "Teaching Experience and Teaching Philosophy",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
